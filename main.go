c28fb8f003dfb993ab3ab5a02c28f06a35cbcc10
DavidEsdrs <carpina1983@gmail.com>
2024-03-01 21:59:58 -0300

    feat: just reading

--- main.go
+++ main.go
@@ -1,33 +1,18 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"time"
 
 	"github.com/DavidEsdrs/1brc/read"
-	"github.com/DavidEsdrs/1brc/write"
 )
 
 func main() {
-	var choose string
-	var size int
-
-	flag.IntVar(&size, "size", 1000, "-size 100")
-	flag.StringVar(&choose, "choose", "", "read")
-
-	flag.Parse()
-
 	var res float64
 
 	start := time.Now()
 
-	switch {
-	case choose == "read":
-		res = read.Execute()
-	case choose == "write":
-		write.Write(size)
-	}
+	res = read.Execute()
 
 	duration := time.Since(start)
 	fmt.Printf("%f - %v", res, duration.String())
